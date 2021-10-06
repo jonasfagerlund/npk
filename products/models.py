4bdc6577e23221cc352c1f1aee3ddf75d2e3944f
jonasafagerlund <52299546+jonasfagerlund@users.noreply.github.com>
2021-10-06 13:02:48 +0200

    added npk to products

--- products/models.py
+++ products/models.py
@@ -7,6 +7,13 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     size = models.DecimalField(max_digits=10, decimal_places=2)
     nicotine_content = models.DecimalField(max_digits=10, decimal_places=2)
+    npk = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        npk = (self.size * self.nicotine_content) / self.price
+        round_npk = round(npk, 2)
+        self.npk = round_npk
+        super(Product, self).save(*args, **kwargs)
