9a1e6e5cfa50efe0bfcc353a84caa85ae5284385
jonasafagerlund <52299546+jonasfagerlund@users.noreply.github.com>
2021-10-06 21:24:51 +0200

    added 'nikotin per dosa'

--- products/models.py
+++ products/models.py
@@ -15,12 +15,14 @@ class Product(models.Model):
     nicotine_content = models.DecimalField(max_digits=10, decimal_places=2)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, blank=True, null=True)
     npk = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
+    npd = models.DecimalField(max_digits=10, decimal_places=2, blank=True)
 
     def __str__(self):
         return self.title
 
     def save(self, *args, **kwargs):
-        npk = (self.size * self.nicotine_content) / self.price
-        round_npk = round(npk, 2)
-        self.npk = round_npk
+        npd = self.size * self.nicotine_content
+        npk = npd / self.price
+        self.npd = npd
+        self.npk = npk
         super(Product, self).save(*args, **kwargs)
